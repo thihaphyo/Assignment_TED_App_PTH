27739a15c30b0815ba21daeeb5497b93beae904f
ThihaPhyo <phyothiha968@gmail.com>
2018-05-29 21:34:17 +0630

    Assignment_TED_APP_PTH

---
+++ app/src/main/java/com/padc/assignment_ted/viewholders/TalkItemViewHolder.java
@@ -0,0 +1,17 @@
+package com.padc.assignment_ted.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ *
+ * Created by Phyo Thiha on 5/29/18.
+ */
+
+public class TalkItemViewHolder extends RecyclerView.ViewHolder {
+
+
+    public TalkItemViewHolder(View itemView) {
+        super(itemView);
+    }
+}
