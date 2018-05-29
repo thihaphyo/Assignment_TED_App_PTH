8ca3fa6022b34ac6831966da3b1d99ea030098b5
ThihaPhyo <phyothiha968@gmail.com>
2018-05-29 21:45:31 +0630

    Add Icon to Toolbar from XML by adding image View to TooBar

--- app/src/main/java/com/padc/assignment_ted/MainActivity.java
+++ app/src/main/java/com/padc/assignment_ted/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        toolbar.setNavigationIcon(R.drawable.ted_logo);
+        //toolbar.setNavigationIcon(R.drawable.ted_logo);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
